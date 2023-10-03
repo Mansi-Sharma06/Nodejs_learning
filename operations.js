2722a7ba625e98ddf661a13bc9ce5b1abcda87f4
Mansi Sharma <78598152+Mansi-Sharma06@users.noreply.github.com>
2023-10-03 21:40:32 +0530

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,15 @@
+module.exports.add = function(a, b) {
+    return a + b;
+}
+// can use export also w/o module, it will work 
+exports.multiply = function (a, b) {
+    return a * b;
+}
+
+exports.subtract = function (a, b) {
+    return a - b;
+}
+
+exports.division = function (a, b) {
+  return a / b;
+};
